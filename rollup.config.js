1c763dd795bdec0c64e842b1ead8d30c0f53fd37
Mikhail Yanzhura <yanzhura@gmail.com>
2022-06-14 15:50:30 +0300

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default {
+    input: './src/index.js',
+    output: {
+        file: './build/bundle.js',
+        format: 'cjs',
+    },
+    watch: {
+        exclude: './node_modules/**',
+    },
+};
